941d62b6b0870c7d3d075b1c9f997396173d3a8c
Ravish Satwani <84343005+ravish036@users.noreply.github.com>
2023-01-07 23:39:08 +0530

    Add files via upload

---
+++ script/navbar.js
@@ -0,0 +1,29 @@
+var menu = document.getElementById("bar");
+var item = document.getElementById("items");
+
+item.style.right = "-300px";
+menu.onclick = function () {
+  if (item.style.right == "-300px") {
+    item.style.right = "0";
+  } else {
+    item.style.right = "-300px";
+  }
+};
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (e) {
+  if (!e.target.matches(".dropbtn")) {
+    var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains("show")) {
+      myDropdown.classList.remove("show");
+    }
+  }
+};
+
+function logOut(){
+  window.location.href="index.html";
+}
